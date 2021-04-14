46b7815933f14c0635eada09bcefa2eda97ad1b5
srinath-sankaragallu <61969343+srinath-sankaragallu@users.noreply.github.com>
2021-04-14 16:56:13 +0530

    Update communicate.py

--- communicate.py
+++ communicate.py
@@ -3,7 +3,7 @@ print("started .....")
 p = subprocess.Popen(['python', 'contin.py'] , stdin = subprocess.PIPE,stdout = subprocess.PIPE , shell = True )
 for i in [1,2,4,9,2,'e']:
     p.stdin.write(bytes(f"{i}\n",'utf-8'))
-    p.stdin.flush()
+    p.stdin.flush() #helps in clearning the stdin buffer
     k = p.stdout.readline()
     print(k)
 p.stdin.close()    
