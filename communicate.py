4a8825e742748b47c0fe21cdafa6980b9542e24d
srinath-sankaragallu <61969343+srinath-sankaragallu@users.noreply.github.com>
2021-04-14 16:51:37 +0530

    Add files via upload

---
+++ communicate.py
@@ -0,0 +1,11 @@
+import subprocess
+print("started .....")
+p = subprocess.Popen(['python', 'contin.py'] , stdin = subprocess.PIPE,stdout = subprocess.PIPE , shell = True )
+for i in [1,2,4,9,2,'e']:
+    p.stdin.write(bytes(f"{i}\n",'utf-8'))
+    p.stdin.flush()
+    k = p.stdout.readline()
+    print(k)
+p.stdin.close()    
+m = p.communicate()[0]
+print(m)
---
+++ continous_printer.py
@@ -0,0 +1,8 @@
+
+while (True):
+    d = input("Enter a number :")
+    if d in ['e','E','exit','EXIT']:
+        break
+    d = int(d)
+    print(f"displaying {d}")
+    print("one iteration over")
